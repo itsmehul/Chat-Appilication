5ae2dc82e477ae9a4ed7fc6b01dbb8afe290a87e
mrgawde <mehulgawdenxt@gmail.com>
2018-03-25 12:52:01 +0530

    Basic setup to SocketIO app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var socket = require('socket.io')
 
 //app setup
 var app = express();
@@ -9,3 +10,11 @@ var server = app.listen(4000, () => {
 
 //Static files
 app.use(express.static('public'));
+
+//Socket setup pass the server we want to work with
+var io = socket(server);
+
+//Socket io waits for a connection to be made. So we listen for that event
+io.on('connection', (socket)=>{
+    console.log('made socket connection', socket.id)
+})
